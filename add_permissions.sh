6d67d47f5be15a0ed91dc56eb1e3962077d34d62
baahk01 <kodjo.baah@bbc.co.uk>
2018-04-04 15:00:44 +0100

    EDPLAT-576 fixed issued with permissions

--- add_permissions.sh
+++ add_permissions.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws lambda add-permission --function-name $1 --statement-id galileoBabel --action "lambda:InvokeFunction" --principal sns.amazonaws.com --source-arn arn:aws:sns:eu-west-1:161201357662:int-galileo-babel-resources-PublishTopic-ASEPFH7ABVQ2 
+aws lambda add-permission --function-name int-editorial-search-galileo-babel --statement-id galileoBabelInt --action "lambda:InvokeFunction" --principal sns.amazonaws.com --source-arn arn:aws:sns:eu-west-1:161201357662:int-galileo-babel-resources-PublishTopic-ASEPFH7ABVQ2 --qualifier int
