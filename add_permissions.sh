2af19ffd201619564c5e295df282c0ee176d0e7e
baahk01 <kodjo.baah@bbc.co.uk>
2018-04-05 10:16:30 +0100

    EDPLAT-576 removed unused scripts

--- add_permissions.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-aws lambda add-permission --function-name int-editorial-search-galileo-babel --statement-id galileoBabelInt --action "lambda:InvokeFunction" --principal sns.amazonaws.com --source-arn arn:aws:sns:eu-west-1:161201357662:int-galileo-babel-resources-PublishTopic-ASEPFH7ABVQ2 --qualifier int
--- subscribe.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-aws sns subscribe  --topic-arn arn:aws:sns:eu-west-1:161201357662:int-galileo-babel-resources-PublishTopic-ASEPFH7ABVQ2  --protocol lambda --notification-endpoint arn:aws:lambda:eu-west-2:$1:function:$2:$3 --region eu-west-1
